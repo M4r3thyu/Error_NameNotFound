8778404847afcaf1cd89e87aaaa44115d4ff64a4
M4r3thyu <45122043+M4r3thyu@users.noreply.github.com>
2018-12-09 17:49:35 +0100

    And und Grundbaustein
    And und Grundbaustein vorerst programmiert

---
+++ Error_NameNotFound/Model/Bausteine.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Error_NameNotFound.Model
+{
+    class Bausteine : INotifyPropertyChanged
+    {
+        protected bool[] input;
+        protected bool[] output;
+        public Bausteine() { }
+        public Bausteine(int input, int output)
+        {
+            this.input = new bool[input];
+            this.output = new bool[output];
+            for (int i = 0; i < input; i++)
+            {
+                this.input[i] = false;
+            }
+            for (int i = 0; i < output; i++)
+            {
+                this.output[i] = false;
+            }
+        }
+        public bool[] Input
+        {
+            get => input;
+            set
+            {
+                if (input != value)
+                {
+                    input = value;
+                    RaisePropertyChanged("Input");
+                    RaisePropertyChanged("Output");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged(string property)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+    class And : Bausteine
+    {
+        public And() { }
+        public And(int input) : base(input, 2)
+        {
+            output[1] = !output[0];
+        }
+        public new bool[] Output {
+            get
+            {
+                bool merke = true;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i] == false)
+                        merke = false;
+                }
+                output[0] = merke;
+                output[1] = !output[0];
+                return output;
+            }
+          }
+    }
+    
+}
