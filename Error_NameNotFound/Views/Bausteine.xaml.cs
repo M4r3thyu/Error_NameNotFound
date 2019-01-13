09f8a7ded25f64a32125fa79f4aeb6fecbe08362
M4r3thyu <45122043+M4r3thyu@users.noreply.github.com>
2019-01-13 11:05:50 +0100

    Requirments Toolbox

---
+++ Error_NameNotFound/Views/Bausteine.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Error_NameNotFound.Views
+{
+    /// <summary>
+    /// Interaction logic for Bausteine.xaml
+    /// </summary>
+    public partial class Bausteine : UserControl
+    {
+        public Bausteine()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+    }
+}
