9597e6347d4f868520c2fc4a66da098a4c77d49b
M4r3thyu <45122043+M4r3thyu@users.noreply.github.com>
2019-01-13 10:57:35 +0100

    Added Easteregg time since releasedate

---
+++ Error_NameNotFound/Model/Releasedate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Error_NameNotFound.Model
+{
+    static class Releasedate
+    {
+        static public double Days_Releasedate()
+        {
+            DateTime releasedate = new DateTime(2019, 03, 26);
+            TimeSpan diff = new TimeSpan();
+            diff =  DateTime.Now - releasedate;
+            return (diff.TotalDays);
+        }
+
+    }
+}
