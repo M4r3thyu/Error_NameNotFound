9dd52fc214281f8d8a0e98cc9f74d2820eb26a5c
M4r3thyu <45122043+M4r3thyu@users.noreply.github.com>
2019-01-14 17:10:21 +0100

    save stuff

---
+++ Error_NameNotFound/ViewModel/Workspace_vm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Error_NameNotFound.Model;
+using System.IO;
+
+namespace Error_NameNotFound.ViewModel
+{
+    [Serializable]
+    class Workspace_vm
+    {
+        List<LogicGates> save;
+        public Workspace_vm()
+        {
+            save = new List<LogicGates>();
+        }
+         public void serializ()
+        {
+                string dir = @"C:\Users\Mar3t\Desktop\Projekt\save";
+                string serializationFile = Path.Combine(dir, "save.bin");
+
+                //serialize
+                using (Stream stream = File.Open(serializationFile, FileMode.Create))
+                {
+                    var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                    bformatter.Serialize(stream, save);
+                }
+
+                //deserialize
+                using (Stream stream = File.Open(serializationFile, FileMode.Open))
+                {
+                    var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                  save = (List<LogicGates>)bformatter.Deserialize(stream);
+                }
+            }
+        }
+    }
