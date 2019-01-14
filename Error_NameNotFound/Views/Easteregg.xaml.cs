d897a3ee285aea23a482c5f7fc20a040555571f4
M4r3thyu <45122043+M4r3thyu@users.noreply.github.com>
2019-01-14 21:03:49 +0100

    Added Easteregg / Added save stuff

---
+++ Error_NameNotFound/Views/Easteregg.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Error_NameNotFound.Model;
+
+namespace Error_NameNotFound.Views
+{
+    /// <summary>
+    /// Interaction logic for Easteregg.xaml
+    /// </summary>
+    public partial class Easteregg : UserControl
+    {
+        public Easteregg()
+        {
+            InitializeComponent();
+            Easteregglabel.Content = null;
+        }
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Easteregglabel.Content = Convert.ToInt32(Releasedate.Days_Releasedate());
+        }
+    }
+}
